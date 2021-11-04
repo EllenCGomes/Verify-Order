b87af441557bd33d29f0b1d993f2a7a70c47f30d
Ellen Gomes <78104233+EllenCGomes@users.noreply.github.com>
2021-11-03 22:25:55 -0300

    Add files via upload

---
+++ EX05 - verify_order.py
@@ -0,0 +1,8 @@
+x = int(input("Enter the first integer: "))
+y = int(input("Enter the second integer: "))
+z = int(input("Enter the third integer: "))
+
+if x<y<z:
+    print("In ascending order")
+else:
+    print("Not in ascending order")
